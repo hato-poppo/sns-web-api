1757399f421e0aed0ddaa2541918289514895b06
hato-poppo <ysng1224@gmail.com>
2021-06-06 22:21:29 +0000

    [Change] Return with user.

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,14 +12,14 @@ class Post < ApplicationRecord
   # Boolean only
   validates :is_deleted, inclusion: { in: [ true, false ] }
 
-  scope :select_posts_with_users, -> { select("posts.*, users.uid, users.name") }
-  scope :join_users, -> { joins(:user) }
+  scope :select_posts, -> { select("posts.*") }
   scope :by_id, -> (id) { where(id: id) }
   scope :by_user_id, -> (user_id) { where(user_id: user_id) }
   scope :by_parent_id, -> (parent_id) { where(parent_id: parent_id) }
   scope :with_visible, -> { where(is_deleted: false) }
   scope :only_parents, -> { where(parent_id: nil) }
   scope :only_children, -> { where('parent_id IS NOT NULL') }
+  scope :with_users, -> { joins(:user).select('users.uid, users.name') }
 
   class << self
 
@@ -29,25 +29,32 @@ class Post < ApplicationRecord
     end
 
     def find_by_id_with_children(id)
-      parent = self.find_by(id: id, is_deleted: false)
-      return nil if parent.blank?
+      parent = self.by_id(id).with_visible.with_users.select_posts
+      return nil if parent.empty?
 
-      children = self.by_parent_id(id).only_children.with_visible
-      parent.children = children
-      parent
+      children = self.by_parent_id(id).only_children.with_visible.with_users.select_posts
+      # Relation で取得する様に変更したら仮想カラムが使えなくなってしまった
+      # parent.children = children
+      # parent
+
+      # Relationは中身が1つの配列みたいな感じになっているので、map や each で回すと良い
+      parent.map { |x| 
+        x.children = children
+        x
+      }.first
     end
 
     def find_by_uid(uid)
       user_id = User.find_by_uid(uid)&.id
-      user_id ? self.by_user_id(user_id).with_visible : nil
+      user_id ? self.by_user_id(user_id).with_visible.with_users.select_posts : nil
     end
 
     def find_by_title(title)
-      self.where('title LIKE ?', "%#{escape(title)}%").with_visible
+      self.where('title LIKE ?', "%#{escape(title)}%").with_visible.with_users.select_posts
     end
 
     def find_by_text(text)
-      self.where('text LIKE ?', "%#{escape(text)}%").with_visible
+      self.where('text LIKE ?', "%#{escape(text)}%").with_visible.with_users.select_posts
     end
 
     def logical_delete_with_children(id)
@@ -61,7 +68,7 @@ class Post < ApplicationRecord
     end
 
     def to_secure
-      { only: [:id, :parent_id, :user_id, :title, :text, :is_deleted, :created_at, :updated_at, :children] }
+      { only: [:id, :parent_id, :user_id, :name, :title, :text, :is_deleted, :created_at, :updated_at, :children] }
     end
 
     private
@@ -71,11 +78,11 @@ class Post < ApplicationRecord
       end
 
       def select_all_visible_parents
-        self.with_visible.only_parents.join_users.select_posts_with_users
+        self.with_visible.only_parents.with_users.select_posts
       end
 
       def select_all_visible_children
-        self.with_visible.only_children.join_users.select_posts_with_users
+        self.with_visible.only_children.with_users.select_posts
       end
 
   end
