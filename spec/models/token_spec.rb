0479550161829288e60a1d72cc6607d3de0391bc
hato-poppo <ysng1224@gmail.com>
2021-06-15 12:49:20 +0000

    [Add] Test for insert_hash.

--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -3,10 +3,17 @@ require 'rails_helper'
 RSpec.describe Token, type: :model do
 
   describe '#insert_hash' do
-    subject { Token.insert_hash('admin', 'aaaa') }
-    context '' do
-      it 'trueが返ること' do
-        is_expected.to eq true
+    subject { Token.insert_hash(uid) }
+    context '指定ユーザーが存在しない場合' do
+      let(:uid) { 'test' }
+      it 'nilが返ること' do
+        is_expected.to eq nil
+      end
+    end
+    context '指定ユーザーが存在する場合' do
+      let(:uid) { 'admin' }
+      it 'トークンハッシュが返ること' do
+        is_expected.to eq Token.find_by_user_id(1).digest_hash
       end
     end
   end
